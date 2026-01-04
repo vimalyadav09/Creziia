73873beca7ada6bdffa0f035e5a609e3cdd5cce2
vimalyadav09 <156642499+vimalyadav09@users.noreply.github.com>
2026-01-04 23:06:25 +0530

    a lot of changes are done

--- nav.js
+++ nav.js
@@ -4,12 +4,14 @@ const navMenu = document.getElementById("nav-menu");
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   navMenu.classList.toggle("active");
+  document.body.classList.toggle("menu-open");
 });
 
-/* Close menu when clicking a link */
+/* Close menu on link click */
 document.querySelectorAll("#nav-menu a").forEach(link => {
   link.addEventListener("click", () => {
     hamburger.classList.remove("open");
     navMenu.classList.remove("active");
+    document.body.classList.remove("menu-open");
   });
 });
